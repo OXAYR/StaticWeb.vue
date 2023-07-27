9b6f5f576684daac3cb43383bdd3870833b3507e
Uzair <uzairmaqsood451@gmail.com>
2023-07-27 20:54:38 +0500

    first Commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-banner avatar="smirk.png" text="..." :stacked="false">
+        <template v-slot:actions>
+            <img/>
+        </template>
+    </v-banner>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BanNer',
+    
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+  
+  </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <NavBar/>
+    <BanNer/>
+    <ItemSLider/>
+    <LandingPage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LandingPage from '@/components/LandingPage.vue'
+import NavBar from '@/components/Nav.vue'
+import BanNer from '@/components/Banner.vue'
+import ItemSLider from '@/components/ItemSlider.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    LandingPage,
+    NavBar,
+    BanNer,
+    ItemSLider,
+  }
+}
+</script>
