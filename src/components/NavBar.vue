222623fb0595533e9153abd26304777c8a40b192
OXAYR <uzairmaqsood451@gmail.com>
2023-07-28 20:14:48 +0500

    Navbar Ready

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,7 @@
 <template>
    <v-toolbar :elevation="4" color="white" height="100">
     <v-btn variant="plain">
+
       <span>
       Start Framing
       </span>
@@ -10,23 +11,26 @@
       How it works
       </span>
     </v-btn>
-    <v-spacer>
-    <v-toolbar-title >
-      <span  class="font-weight-regular text-h4">Hang
-      </span>
+    <v-toolbar-title>
+      <img src="../assets/hank-logo.svg" alt="hank.logo" width="125" height="125" style="margin-right: 90px;">
       </v-toolbar-title>
-      </v-spacer>
       <v-btn variant="plain">
       <span>
       Support
       </span>
     </v-btn>
-    
     <v-btn variant="plain">
       <v-icon
       size="large"
-      color="blue-darken-2"
-      icon="mdi-message-text"
+      color="black"
+      icon="mdi-shopping"
+    ></v-icon>
+    </v-btn>
+    <v-btn variant="plain">
+      <v-icon
+      size="large"
+      color="black"
+      icon="mdi-account"
     ></v-icon>
     </v-btn>
     </v-toolbar>
