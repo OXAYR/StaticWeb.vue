d03a6c4df596fce128fc990c6165981c86ac0fe7
OXAYR <uzairmaqsood451@gmail.com>
2023-07-28 19:40:51 +0500

    Navbar Commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+   <v-toolbar :elevation="4" color="white" height="100">
+    <v-btn variant="plain">
+      <span>
+      Start Framing
+      </span>
+    </v-btn>
+    <v-btn variant="plain">
+      <span>
+      How it works
+      </span>
+    </v-btn>
+    <v-spacer>
+    <v-toolbar-title >
+      <span  class="font-weight-regular text-h4">Hang
+      </span>
+      </v-toolbar-title>
+      </v-spacer>
+      <v-btn variant="plain">
+      <span>
+      Support
+      </span>
+    </v-btn>
+    
+    <v-btn variant="plain">
+      <v-icon
+      size="large"
+      color="blue-darken-2"
+      icon="mdi-message-text"
+    ></v-icon>
+    </v-btn>
+    </v-toolbar>
+  </template>
+  <script>
+    export default {
+        name: 'NavBar',
+  
+    }
+  </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="home">
+  <v-app>
     <NavBar/>
-    <BanNer/>
-    <ItemSLider/>
-    <LandingPage msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-content>
+      asdasdsdasdas
+      <BanNer/>
+      <ItemSLider/>
+      <LandingPage msg="Welcome to Your Vue.js App"/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
 // @ is an alias to /src
 import LandingPage from '@/components/LandingPage.vue'
-import NavBar from '@/components/Nav.vue'
+import NavBar from '@/components/NavBar.vue'
 import BanNer from '@/components/Banner.vue'
 import ItemSLider from '@/components/ItemSlider.vue'
 
