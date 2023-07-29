02231b1764f9a1dc7e6d3024f44c6feddd6956d3
OXAYR <uzairmaqsood451@gmail.com>
2023-07-29 17:34:49 +0500

    Just footer left

---
+++ src/components/ButtonComp.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-content>
+        <v-btn class="ma-7 text-none" variant="tonal" rounded="xl" size="x-large" color="yellow-darken-4" >{{ text }}</v-btn>
+    </v-content>    
+</template>
+<script>
+    export default {
+        name: "ButtonComp",
+        props:{
+            text :{
+                type: String,
+                required: true,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+/* .custom-button:{
+    color: black;
+} */
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,7 @@
       <ItemCard text="Wire-Brushed bare wood" header="Brancaster" 
       :imageSrc="require('../assets/Slide_item.jpg')"
       imageAlt="ItemCard"/>
+      <ButtonComp text="Design a custom frame"/>
 
     <div style="background-color:rgba(245,245,244);">
       <BanNer msg="Cost effective readymade sizes. 
@@ -18,19 +19,21 @@
       build, and dispatch your frame the next day. Ideal for quick gifts." header="Standard sizes & gift frames" 
       :imageSrc="require('../assets/lowerContent.png')"
       imageAlt="LowerBanner"/>
-
+      
       <ItemCard text="20x30cm textured bare wood frame" header="Small Gift Frame in Brancaster (P)" 
       :imageSrc="require('../assets/Slide_item-2.jpg')"
       imageAlt="ItemCard" price="£34.95"/>
-
+      <ButtonComp text="Shop Standard Frames"/>
+      
       <BanNer msg="Want that statement gallery wall without the hassle? 
       Choose one of our gallery walls and upload your photos in minutes, 
       or get one of our designers to help create a truly custom one. Coming soon!" header="Gallery walls" 
       :imageSrc="require('../assets/lowerContent-2.png')"
       imageAlt="LowerBanner2"/>
+      <ButtonComp text="Coming Soon!"/>
     </div> 
     </v-content>
-
+    <FooterComp/>
   </v-app>
 </template>
 
@@ -40,6 +43,8 @@
 import NavBar from '@/components/NavBar.vue'
 import BanNer from '@/components/Banner.vue'
 import ItemCard from '@/components/ItemCard.vue'
+import ButtonComp from '@/components/ButtonComp.vue'
+import FooterComp from '@/components/FooterComp.vue'
 
 export default {
   name: 'HomeView',
@@ -47,6 +52,8 @@ export default {
     NavBar,
     BanNer,
     ItemCard,
+    ButtonComp,
+    FooterComp
   }
 }
 </script>
